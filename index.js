aa437b2fbeaa23c3c8751cd626e22319e822f332
luhnvalid <84154628+luhnvalid@users.noreply.github.com>
2021-05-13 23:55:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,55 @@
+function generate(number){
+
+    if (!number.includes("*")){
+        return number;
+    }
+
+    let limit = 0;
+    for (i = 0; i < number.length; i++){
+        if (number.charAt(i) === '*'){
+            limit = i;
+            break
+        }
+    }
+
+    prefix = number.substring(0,limit);
+
+    const spaces = number.length - limit;
+
+    let numbers = '';
+    if (spaces === 1){
+        for (i = 0; i < 9; i++){
+            if (isLuhnValid(prefix + i) === 'VALID'){
+                numbers = numbers + prefix + i + ', ';
+            }
+        }
+    } else if (spaces === 2){
+        for (i = 0; i < 9; i++){
+            for (j = 0; j < 9; j++)
+            if (isLuhnValid(prefix + i + j) === 'VALID'){
+                numbers = numbers + prefix + i + j + ', ';
+            }
+        }
+    } else if (spaces === 3){
+        for (i = 0; i < 9; i++){
+            for (j = 0; j < 9; j++) {
+                for (k = 0; k < 9; k++) {
+                    if (isLuhnValid(prefix + i + j + k) === 'VALID') {
+                        numbers = numbers + prefix + i + j + k + ', ';
+                    }
+                }
+            }
+        }
+    } else {
+        return "Maximum 3 empty characters"
+    }
+
+    return numbers.substring(0, numbers.length - 2)
+}
 function isLuhnValid(value) {
 
     if (/[^0-9-\s]+/.test(value)) {
-        setAnswer("NO");
-        return false
+        return "INVALID";
     }
 
     // The Luhn Algorithm.
@@ -23,19 +70,27 @@ function isLuhnValid(value) {
     luhnValid = (nCheck % 10) === 0;
 
     if (luhnValid) {
-        setAnswer("YES");
-        return false
+        return "VALID";
     } else {
-        setAnswer("NO");
-        return false
+        return "INVALID";
     }
 }
 
 
 function setAnswer(answer) {
-    document.getElementById("isLuhnValidAnswer").textContent = answer
+    document.getElementById("answer").textContent = answer
 }
 
-function startProcessing(){
-    document.getElementById("isLuhnValidAnswer").textContent = "o dada"
+function startProcessing() {
+    const queryString = window.location.search.substring(1);
+    const mode = queryString.split("&")[0];
+    const number = queryString.split("&")[1];
+
+    if (mode === 'valid') {
+        setAnswer(isLuhnValid(number));
+    }else if (mode === 'generate'){
+        setAnswer(generate(number))
+    }else{
+        setAnswer('error')
+    }
 }
